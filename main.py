2ac19843905f91da2e407ef3979c48e34364e887
Rina Ralijaona <116346251+rinarlj@users.noreply.github.com>
2024-12-17 15:00:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,8 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
 
-    # --- Étape 5 : Construction et entraînement du modèle ---
     classifier.build_model(bidirectional=True)
     classifier.train(X_train, y_train, X_val, y_val, epochs=5)
 
-    # --- Étape 6 : Évaluation sur les données de test ---
     accuracy = classifier.evaluate(X_test, y_test)
     print(f"Précision finale sur les données de test : {accuracy:.4f}")
